8a5c020408a85751a62e7df9429de7bd86c2e208
baldurt1992 <andresbaldur92@gmail.com>
2024-07-13 11:24:27 -0500

    feature/adashboard: dashboard admin desgin was added. configuration tailwind theme, routes path dashboad configured.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('./components/layout/layout.component'),
+    children: [
+      {
+        path: 'admindashboard',
+        loadComponent: () =>
+          import('./view/admin-dashboard/admin-dashboard.component'),
+      },
+      {
+        path: 'profile',
+        loadComponent: () => import('./components/profile/profile.component'),
+      },
+      {
+        path: 'charts',
+        loadComponent: () => import('./components/charts/charts.component'),
+      },
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}"],
+  content: ["./src/**/*.{html,ts}", "/src/styles.css"],
   theme: {
-    extend: {},
+    colors: {
+      white: "#ffffff",
+      primaryDark: "#000066",
+      primaryClear: "#00A9E0",
+      secondary: "#F7B224",
+      black: "#000000",
+      grayDark: "#191B20",
+    },
+    extend: {
+      transitionProperty: {
+        multiple: "width , height , backgroundColor , border-radius",
+      },
+      transitionDuration: {
+        2000: "2000ms",
+      },
+      text: {
+        lg: "1.5rem",
+      },
+    },
   },
-  color: {
-    white: "#ffffff",
+  variants: {
+    extend: {
+      backgroundColor: ["active", "hover"],
+    },
   },
+
   plugins: [],
 };
