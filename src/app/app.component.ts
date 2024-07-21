8ccc5c1409e1b53c426eee4227861000147d51c0
baldurt1992 <andresbaldur92@gmail.com>
2024-07-21 12:57:31 -0500

    commit for update dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from './view/register/register.component';
 import { FormRegisterComponent } from './components/form-register/form-register.component';
+import { HomeComponent } from './view/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RegisterComponent, FormRegisterComponent],
+  imports: [
+    RouterOutlet,
+    RegisterComponent,
+    FormRegisterComponent,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
---
+++ src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,11 @@
+import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
+import { lyl, StyleRenderer } from '@alyle/ui';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.css',
+})
+export class CarouselComponent {}
