93ea2ba76f5c9b8246b0a579379684a7290fcbed
baldurt1992 <andresbaldur92@gmail.com>
2024-07-22 13:39:41 -0500

    Header and banner home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ApplicationConfig, Component } from '@angular/core';
+import { provideRouter, RouterOutlet } from '@angular/router';
 import { RegisterComponent } from './view/register/register.component';
 import { FormRegisterComponent } from './components/form-register/form-register.component';
 import { HomeComponent } from './view/home/home.component';
+import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+export const appConfig: ApplicationConfig = {
+  providers: [provideRouter(routes), provideAnimationsAsync()],
+};
 @Component({
   selector: 'app-root',
   standalone: true,
--- src/app/components/carousel/carousel.component.ts
+++ src/app/components/carousel/carousel.component.ts
@@ -1,11 +1,38 @@
-import { Component, ChangeDetectionStrategy, inject } from '@angular/core';
-import { lyl, StyleRenderer } from '@alyle/ui';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrl: './carousel.component.css',
+  styleUrls: ['./carousel.component.css'],
 })
-export class CarouselComponent {}
+export class CarouselComponent implements OnInit {
+  slides = [
+    { src: './img/banner.jpg' },
+    { src: './img/banner1.jpg' },
+    { src: './img/banner2.jpg' },
+  ];
+
+  activeSlideIndex = 0;
+
+  ngOnInit(): void {
+    this.startAutoSlide();
+  }
+
+  startAutoSlide(): void {
+    setInterval(() => {
+      this.nextSlide();
+    }, 5000);
+  }
+
+  nextSlide(): void {
+    this.activeSlideIndex = (this.activeSlideIndex + 1) % this.slides.length;
+  }
+
+  prevSlide(): void {
+    this.activeSlideIndex =
+      (this.activeSlideIndex - 1 + this.slides.length) % this.slides.length;
+  }
+}
