d92bf9277c45ada7594dda723ca1a4267a28af3d
Camilavelez18 <camilavelez1218@gmail.com>
2024-07-22 13:41:23 -0500

    login-form

---
+++ src/app/view/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormLoginComponent } from "../../components/form-login/form-login.component";
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [FormLoginComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
