a81e2ee9f37cc085ae4ef3987ff31b30fe2ccbec
Camilavelez18 <camilavelez1218@gmail.com>
2024-07-26 19:31:32 -0500

    ts form-register-validator

--- src/app/components/form-register/form-register.component.ts
+++ src/app/components/form-register/form-register.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { FormBuilder,FormGroup,Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form-register',
@@ -8,4 +9,26 @@ import { RouterLink } from '@angular/router';
   templateUrl: './form-register.component.html',
   styleUrl: './form-register.component.css',
 })
-export class FormRegisterComponent {}
+export class FormRegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.registerForm = this.fb.group({
+      typePerson: ["" , Validators.required],
+      fullName: ["" , Validators.required],
+      factory: ["" , Validators.required],
+      businessName: [""],
+      email: ["" ,[Validators.required, Validators.email]],
+      password: ["" , Validators.required],
+      comfirmPassword: ["" , Validators.required],
+    });
+  }
+
+
+  onSubmit(){
+    if (this.registerForm.valid) {
+      
+    }
+  }
+}
+
