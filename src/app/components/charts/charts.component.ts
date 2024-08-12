403e457e8bbce4f82220b7ed15c3271e2ebc1640
Esteban Montoya <emontoyab93@gmail.com>
2024-08-12 00:27:46 -0500

    move out the old charts directory

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ChartRelojEstrategicoComponent } from '../chart-reloj-estrategico/chart-reloj-estrategico.component';
+import { ChartCirculoDoradoComponent } from '../chart-circulo-dorado/chart-circulo-dorado.component';
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [ChartRelojEstrategicoComponent],
+  imports: [ChartRelojEstrategicoComponent,
+    ChartCirculoDoradoComponent
+  ],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css',
 })
