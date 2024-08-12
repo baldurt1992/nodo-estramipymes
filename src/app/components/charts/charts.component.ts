9ff43950a3a66b6b32fe7df0c7275a461d6c2458
Esteban Montoya <emontoyab93@gmail.com>
2024-08-11 22:22:46 -0500

    create reloj-estrategico chart

--- src/app/components/charts/charts.component.ts
+++ src/app/components/charts/charts.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ChartRelojEstrategicoComponent } from '../chart-reloj-estrategico/chart-reloj-estrategico.component';
 
 @Component({
   selector: 'app-charts',
   standalone: true,
-  imports: [RouterLink],
+  imports: [ChartRelojEstrategicoComponent],
   templateUrl: './charts.component.html',
   styleUrl: './charts.component.css',
 })
