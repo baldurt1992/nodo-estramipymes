336b5b02b15380bea7052eaabb07cda9114adc1c
baldurt1992 <andresbaldur92@gmail.com>
2024-08-13 19:59:49 -0500

    change charts

--- src/app/components/chart-circulo-dorado/chart-circulo-dorado.component.ts
+++ src/app/components/chart-circulo-dorado/chart-circulo-dorado.component.ts
@@ -8,22 +8,19 @@ Chart.register(annotationPlugin);
   selector: 'app-chart-circulo-dorado',
   standalone: true,
   templateUrl: './chart-circulo-dorado.component.html',
-  styleUrls: ['./chart-circulo-dorado.component.css']
+  styleUrls: ['./chart-circulo-dorado.component.css'],
 })
 export class ChartCirculoDoradoComponent implements AfterViewInit {
-
   ngAfterViewInit(): void {
     this.createChart();
     this.setTextContent();
   }
 
   createChart(): void {
-    const data = [
-      { precio: 50, valor: 50 }
-    ];
+    const data = [{ precio: 50, valor: 50 }];
 
     const circle_size = [5, 95, 5, 95];
-    const diameter = (circle_size[1] - circle_size[0]);
+    const diameter = circle_size[1] - circle_size[0];
     const k = 0.15;
     const percent_circle = diameter * k;
     const font_size = 15;
@@ -40,7 +37,7 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
           min: 0,
           max: 100,
           display: false,
-        }
+        },
       },
       plugins: {
         title: {
@@ -50,7 +47,7 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
           align: 'center',
         },
         legend: {
-          display: false
+          display: false,
         },
         annotation: {
           annotations: {
@@ -60,8 +57,8 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
               yMax: circle_size[1],
               xMin: circle_size[2],
               xMax: circle_size[3],
-              borderColor: '#ed7d31',
-              backgroundColor: '#ed7d31',
+              borderColor: '#FD673E',
+              backgroundColor: '#FD673E',
               borderWidth: 2,
             },
             how_circle: {
@@ -70,18 +67,18 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
               yMax: circle_size[1] - percent_circle,
               xMin: circle_size[2] + percent_circle,
               xMax: circle_size[3] - percent_circle,
-              borderColor: '#ffc000',
-              backgroundColor: '#ffc000',
+              borderColor: '#F7B224',
+              backgroundColor: '#F7B224',
               borderWidth: 2,
             },
             why_circle: {
               type: 'ellipse',
-              yMin: circle_size[0] + (percent_circle * 2),
-              yMax: circle_size[1] - (percent_circle * 2),
-              xMin: circle_size[2] + (percent_circle * 2),
-              xMax: circle_size[3] - (percent_circle * 2),
-              borderColor: '#4472c4',
-              backgroundColor: '#4472c4',
+              yMin: circle_size[0] + percent_circle * 2,
+              yMax: circle_size[1] - percent_circle * 2,
+              xMin: circle_size[2] + percent_circle * 2,
+              xMax: circle_size[3] - percent_circle * 2,
+              borderColor: '#085FA0',
+              backgroundColor: '#085FA0',
               borderWidth: 2,
             },
             line_why: {
@@ -127,8 +124,8 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
               backgroundColor: 'rgba(245,245,245,0)',
               content: ['¿Por qué?', '¿Para qué?'],
               font: {
-                size: font_size
-              }
+                size: font_size,
+              },
             },
             label_how: {
               type: 'label',
@@ -137,8 +134,8 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
               backgroundColor: 'rgba(245,245,245,0)',
               content: ['¿Cómo?'],
               font: {
-                size: font_size
-              }
+                size: font_size,
+              },
             },
             label_what: {
               type: 'label',
@@ -147,12 +144,12 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
               backgroundColor: 'rgba(245,245,245,0)',
               content: ['¿Qué?'],
               font: {
-                size: font_size
-              }
+                size: font_size,
+              },
             },
-          }
-        }
-      }
+          },
+        },
+      },
     };
 
     const config: ChartConfiguration = {
@@ -161,9 +158,9 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
         datasets: [
           {
             label: 'Círculo Dorado',
-            data: data.map(row => ({ x: row.precio, y: row.valor }))
+            data: data.map((row) => ({ x: row.precio, y: row.valor })),
           },
-        ]
+        ],
       },
       options,
     };
@@ -174,17 +171,20 @@ export class ChartCirculoDoradoComponent implements AfterViewInit {
     );
   }
 
-setTextContent(): void {
-  // Text Circulo Dorado - Why
-  const p_why: string = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum nisi at ipsum mattis, at gravida dui ornare. Aliquam odio ex, ullamcorper eu arcu sed, tincidunt viverra lacus. Etiam posuere elit non accumsan egestas. In sodales cursus dignissim. Curabitur sed consequat mi. Nullam eleifend justo nec lorem ultricies, at feugiat nunc dignissim. Vestibulum quis bibendum massa. Praesent nec euismod turpis, id laoreet urna. Aenean ullamcorper neque et lobortis fringilla.";
-  (document.getElementById('p_why') as HTMLElement).textContent = p_why;
+  setTextContent(): void {
+    // Text Circulo Dorado - Why
+    const p_why: string =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce bibendum nisi at ipsum mattis, at gravida dui ornare. Aliquam odio ex, ullamcorper eu arcu sed, tincidunt viverra lacus. Etiam posuere elit non accumsan egestas. In sodales cursus dignissim. Curabitur sed consequat mi. Nullam eleifend justo nec lorem ultricies, at feugiat nunc dignissim. Vestibulum quis bibendum massa. Praesent nec euismod turpis, id laoreet urna. Aenean ullamcorper neque et lobortis fringilla.';
+    (document.getElementById('p_why') as HTMLElement).textContent = p_why;
 
-  // Text Circulo Dorado - How
-  const p_how: string = "Proin pharetra sem ligula, vitae tempus ex pharetra quis. Aliquam erat volutpat. Aliquam sapien sapien, interdum id erat eu, vestibulum feugiat purus. Donec euismod tempor luctus. In hac habitasse platea dictumst. Pellentesque nulla nibh, rhoncus ac dignissim eu, maximus interdum felis. Donec tortor ex, dapibus sit amet metus et, lobortis porta nisl.";
-  (document.getElementById('p_how') as HTMLElement).textContent = p_how;
+    // Text Circulo Dorado - How
+    const p_how: string =
+      'Proin pharetra sem ligula, vitae tempus ex pharetra quis. Aliquam erat volutpat. Aliquam sapien sapien, interdum id erat eu, vestibulum feugiat purus. Donec euismod tempor luctus. In hac habitasse platea dictumst. Pellentesque nulla nibh, rhoncus ac dignissim eu, maximus interdum felis. Donec tortor ex, dapibus sit amet metus et, lobortis porta nisl.';
+    (document.getElementById('p_how') as HTMLElement).textContent = p_how;
 
-  // Text Circulo Dorado - What
-  const p_what: string = "Sed nec nulla non nunc egestas ultrices. Pellentesque purus erat, mattis vitae semper et, dapibus sit amet nisl. In mollis lorem leo. Pellentesque luctus maximus purus, non viverra tortor molestie sit amet. Etiam eu nulla a diam mollis sagittis. Cras fringilla ut mauris vitae hendrerit. Pellentesque ac turpis velit.";
-  (document.getElementById('p_what') as HTMLElement).textContent = p_what;
-}
+    // Text Circulo Dorado - What
+    const p_what: string =
+      'Sed nec nulla non nunc egestas ultrices. Pellentesque purus erat, mattis vitae semper et, dapibus sit amet nisl. In mollis lorem leo. Pellentesque luctus maximus purus, non viverra tortor molestie sit amet. Etiam eu nulla a diam mollis sagittis. Cras fringilla ut mauris vitae hendrerit. Pellentesque ac turpis velit.';
+    (document.getElementById('p_what') as HTMLElement).textContent = p_what;
+  }
 }
